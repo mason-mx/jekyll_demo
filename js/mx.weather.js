5472bd00cace34f0ea83dabd24a05992db96bb28
mason <star.wang@live.com>
2018-08-30 22:46:19 +1200

    add options and handle resize event

--- js/mx.weather.js
+++ js/mx.weather.js
@@ -1,65 +1,129 @@
 (function($) {
     $.widget("mx.weather", {
         options: {
+            citysDict: {},
+            country: "NZ",
             city: "Auckland",
             unit: "C",
-            location: "bottom",
+            width: 0,
             color: "#fff",
-            backgroundColor: "#000"
+            backgroundColor: "#000",
+            city_full: "",
+            temperature_string: "",
+            temp_f: "",
+            temp_c: "",
+            weather: "",
+            time: ""
         },
+
         _create: function() {
             var self = this, o = self.options, el = self.element;
 
-            var icon = $("<div></div>")
+            o.citysDict = {
+                "Auckland":   "NZ",
+                "Willington": "NZ"
+            };
+
+            $("<div></div>")
                 .attr("id", "icon")
                 .appendTo(el);
-            var city = $("<div></div>")
+            $("<div></div>")
                 .attr("id", "city")
-                .append("<strong></strong>")
                 .appendTo(el);
-            var weather = $("<div></div>")
+            $("<strong></strong>")
+                .appendTo(city);
+            $("<div></div>")
                 .attr("id", "weather")
                 .appendTo(el);
-            var time = $("<div></div>")
+            $("<div></div>")
                 .attr("id", "time")
                 .appendTo(el);
 
-            var url = 'https://api.wunderground.com/api/c6dc8e785d943109/conditions/q/NZ/'+ o.city + '.json'
-
-            $.ajax(url, {
-                dataType: 'jsonp',
-                success: function(json) {
-                    console.log(json.current_observation.weather);
-                    city.text(json.current_observation.display_location.full)
-                    icon.html('<img src=' + json.current_observation.icon_url + '>')
-                    weather.text(json.current_observation.temperature_string + " " + json.current_observation.weather);
-                    time.text(json.current_observation.observation_time_rfc822);
-                }
-            });
-
             el.css({
                 position: "fixed",
-                bottom: "60px",
-                right: 0,
-                //margin-left: "10px",
-                //width: capWidth,
-                //top: (o.location === "top") ? el.offset().top : el.offset().top + el.height() - capHeight,
-                //left: el.offset().left,
-                //display: "block"
+                bottom: "30px",
+                right: 0
             });
+
+
+            this._fetchWeather(this._constrain(o.city));
                      
             $(window).resize(function(){
-                el.css({
-                    top: (o.location === "top") ? el.offset().top : el.offset().top + el.height() - capHeight,
-                    left: el.offset().left
-                });
+                //el.css({
+                //    bottom: "50px"
+                //});
+                self._updateElements();
             });
         },
+
         destroy: function() {           
             this.element.next().remove();
                      
             $(window).unbind("resize");
         },
+
+        // create a public method
+        city: function(value) {
+            // no value passed, act as a getter
+            if (value === undefined) {
+                return this.options.city;
+            // value passed, act as a setter
+            } else {
+                this.options.city = this._constrain(value);
+                this._fetchWeather(this.options.city);
+            }
+        },
+
+        // create a private method
+        _constrain: function(city) {
+            var dict = this.options.citysDict;
+            var value = "";
+            if (dict[city] === null) {
+                value = dict["Auckland"] + "/Auckland";
+            }
+            else {
+                value = dict[city] + "/" + city;
+            }
+            return value;
+        },
+
+        // create a private method
+        _fetchWeather: function(subURL) {
+            var self = this, o = self.options;
+            var url = 'https://api.wunderground.com/api/c6dc8e785d943109/conditions/q/'+ subURL + '.json'
+
+            $.ajax(url, {
+                dataType: 'jsonp',
+                success: function(json) {
+                    console.log(json.current_observation.weather);
+                    o.city_full = json.current_observation.display_location.full;
+                    o.icon_url = '<img src=' + json.current_observation.icon_url + '>';
+                    o.temperature_string = json.current_observation.temperature_string;
+                    o.temp_f = json.current_observation.temp_f;
+                    o.temp_c = json.current_observation.temp_c;
+                    o.weather = json.current_observation.weather;
+                    o.time = json.current_observation.observation_time_rfc822;
+                    self._updateElements();
+                }
+            });
+        },
+
+        _updateElements: function() {
+            var o = this.options;
+            if ($(window).width() > 660) {
+                $("#city > strong").text(o.city_full);
+                $("#icon").html(o.icon_url);
+                $("#weather").text(o.temperature_string + " " + o.weather);
+                $("#time").text(o.time);
+             }
+             else {
+                $("#city > strong").text(o.city);
+                $("#icon").html(o.icon_url);
+                $("#weather").text(o.temp_c + " " + o.weather);
+                $("#time").text("");
+             }
+        },
+
         _setOption: function(option, value) {
             $.Widget.prototype._setOption.apply( this, arguments );
          
